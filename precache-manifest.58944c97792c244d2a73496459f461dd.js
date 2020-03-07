c63705192d6a523d4c2ebbfba66318b3bc6613f6
Debora Carvalho <debora14leite@gmail.com>
2020-03-07 19:33:23 -0300

    Update

---
+++ precache-manifest.58944c97792c244d2a73496459f461dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44026ceeae49fb7cad0dc80464f2794d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ee8af536320582dcd7",
+    "url": "/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "badb2636bd56744507fa",
+    "url": "/static/js/2.417f3472.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.417f3472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ee8af536320582dcd7",
+    "url": "/static/js/main.11ee6798.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
